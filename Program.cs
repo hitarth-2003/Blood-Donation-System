322cac07ae523e7002eef64bd9037c895fb58ead
hitarth-2003 <hitarthchoubisa9680@gmail.com>
2026-07-27 23:25:37 +0530

    new commit

--- Program.cs
+++ Program.cs
@@ -1,24 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using BloodDonationSystem.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Database Connection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+
 app.UseHttpsRedirection();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapStaticAssets();
+
 
 app.MapControllerRoute(
     name: "default",
