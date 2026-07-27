53385aa461a0c1f99333712ccaef8e08f02be33d
hitarth-2003 <hitarthchoubisa9680@gmail.com>
2026-07-27 23:15:59 +0530

    new commit

---
+++ Models/Contact.cs
@@ -0,0 +1,17 @@
+namespace BloodDonationSystem.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
