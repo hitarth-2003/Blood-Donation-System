9dc2583e10c0cf52da14d0c6ac1ba8d61126247b
hitarth-2003 <hitarthchoubisa9680@gmail.com>
2026-07-27 23:08:08 +0530

    new commit 27july

--- obj/Debug/net10.0/BloodDonationSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/BloodDonationSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloodDonationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9fa263c80b6bd2735d8201de7796a4ce73cc6bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36c8a7f50716179365775123dde9331f5e4a7b75")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloodDonationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloodDonationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
