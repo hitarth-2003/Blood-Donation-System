dc77c633295bc86d9bfa9eb3b8f3c35d648486a4
hitarth-2003 <hitarthchoubisa9680@gmail.com>
2026-07-27 23:15:15 +0530

    new commit done

---
+++ Models/BloodRequest.cs
@@ -0,0 +1,29 @@
+namespace BloodDonationSystem.Models
+{
+    public class BloodRequest
+    {
+        public int Id { get; set; }
+
+        public string PatientName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; }
+
+        public string BloodGroup { get; set; }
+
+        public int UnitsRequired { get; set; }
+
+        public string HospitalName { get; set; }
+
+        public string City { get; set; }
+
+        public string ContactNumber { get; set; }
+
+        public DateTime RequiredDate { get; set; }
+
+        public string Priority { get; set; }
+
+        public string Address { get; set; }
+    }
+}
